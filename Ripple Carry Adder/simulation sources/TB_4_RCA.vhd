765876da4e34a7863f9882ac8b95c73f73cdf024
lukezzz <patalee2001@gmail.com>
2023-03-25 14:17:46 +0530

    added

--- Ripple Carry Adder/simulation sources/TB_4_RCA.vhd
+++ Ripple Carry Adder/simulation sources/TB_4_RCA.vhd
@@ -37,62 +37,63 @@ end TB_4_RCA;
 
 architecture Behavioral of TB_4_RCA is
 
-COMPONENT RCA_4
- PORT( A0,A1,A2,A3,B0,B1,B2,B3,C_in : IN STD_LOGIC;
- S0,S1,S2,S3,C_out : OUT STD_LOGIC);
- END COMPONENT;
-SIGNAL A0,A1,A2,A3,B0,B1,B2,B3,C_in : std_logic;
-  SIGNAL S0,S1,S2,S3,C_out : std_logic;
-begin
-UUT: RCA_4 PORT MAP(
-A0 => A0,
-A1 => A1,
-A2 => A2,
-A3 => A3,
-B0 => B0,
-B1 => B1,
-B2 => B2,
-B3 => B3,
-C_in =>C_in,
-S0 => S0,
-S1 => S1,
-S2 => S2,
-S3 => S3,
-C_out => C_out
+  COMPONENT RCA_4
+      PORT( A0,A1,A2,A3,B0,B1,B2,B3,C_in : IN STD_LOGIC;
+      S0,S1,S2,S3,C_out : OUT STD_LOGIC);
+  END COMPONENT;
 
- );
-process
-  begin
-  A0 <= '0';
-  A1 <= '1'; 
-  A2 <= '1';
-  A3 <= '1'; 
-  B0 <= '1';
-  B1 <= '1'; 
-  B2 <= '1'; 
-  B3 <= '0';
- C_in <= '0';
-  WAIT FOR 100 ns; -- after 100 ns change inputs
- A2 <= '0';
- B0 <= '0';
-   B1 <= '0'; 
-   B3 <= '1'; 
-  WAIT FOR 100 ns; --change again
-  B0 <= '1';
- 
-  WAIT FOR 100 ns; --change again
- A2 <= '1';
-    B1 <= '1';
-  WAIT FOR 100 ns; --change again
-  A0 <= '1';
+  SIGNAL A0,A1,A2,A3,B0,B1,B2,B3,C_in : std_logic;
+  SIGNAL S0,S1,S2,S3,C_out : std_logic;
+
+begin
+  UUT: RCA_4 PORT MAP(
+    A0 => A0,
+    A1 => A1,
+    A2 => A2,
+    A3 => A3,
+    B0 => B0,
+    B1 => B1,
+    B2 => B2,
+    B3 => B3,
+    C_in =>C_in,
+    S0 => S0,
+    S1 => S1,
+    S2 => S2,
+    S3 => S3,
+    C_out => C_out
+
+  );
+  process
+    begin
+      A0 <= '0';
+      A1 <= '1'; 
+      A2 <= '1';
+      A3 <= '1'; 
+      B0 <= '1';
+      B1 <= '1'; 
+      B2 <= '1'; 
+      B3 <= '0';
+      C_in <= '0';
+    WAIT FOR 100 ns; -- after 100 ns change inputs
+      A2 <= '0';
+      B0 <= '0';
+      B1 <= '0'; 
+      B3 <= '1'; 
+    WAIT FOR 100 ns; --change again
+      B0 <= '1';
   
-  WAIT FOR 100 ns; --change again
- B3 <= '0';
-  WAIT FOR 100 ns; --change again
-  B2 <= '0';
-  WAIT FOR 100 ns; --change again
- B1 <= '0';
-  WAIT; -- will wait forever
-  end process;
+    WAIT FOR 100 ns; --change again
+      A2 <= '1';
+      B1 <= '1';
+    WAIT FOR 100 ns; --change again
+      A0 <= '1';
+    WAIT FOR 100 ns; --change again
+      B3 <= '0';
+    WAIT FOR 100 ns; --change again
+      B2 <= '0';
+    WAIT FOR 100 ns; --change again
+      B1 <= '0';
+    WAIT; -- will wait forever
+    end process;
 
 end Behavioral;
